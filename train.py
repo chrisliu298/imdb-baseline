c365cde3b50212d47f394174fd341c2d60399906
chrisliu298 <yuhaoliu298@gmail.com>
2022-07-31 23:02:22 -0700

    Move data split size logging to model

--- train.py
+++ train.py
@@ -101,13 +101,6 @@ def main():
         benchmark=True,
         logger=logger,
     )
-    wandb.log(
-        {
-            "train_size": len(datamodule.train_dataset),
-            "val_size": len(datamodule.val_dataset),
-            "test_size": len(datamodule.test_dataset),
-        }
-    )
     # train
     trainer.fit(model=model, datamodule=datamodule)
     trainer.test(model=model, datamodule=datamodule, verbose=config.verbose)
